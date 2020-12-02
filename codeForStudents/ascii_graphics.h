403733f829792c0276fcb4ebbcfbac63ec544d01
David Kim <j626kim@uwaterloo.ca>
2020-12-01 21:49:33 -0500

    added codeForStudents

---
+++ codeForStudents/ascii_graphics.h
@@ -0,0 +1,63 @@
+#include <vector>
+#include <string>
+
+//SIMPLE_GRAPHICS = 0 displays a fancy style.
+//SIMPLE_GRAPHICS = 1 displays the style shown in the project specification's examples
+//You are encouraged to use SIMPLE_GRAPHICS = 0, but some terminals may not like it.
+#define SIMPLE_GRAPHICS 0
+
+typedef std::vector<std::string> card_template_t;
+
+
+// Returns a vector of strings to display a simple minion with no abilities
+card_template_t display_minion_no_ability(std::string name,int cost,int attack,int defence);
+
+// Returns a vector of strings to display a minion with a triggered ability
+card_template_t display_minion_triggered_ability(std::string name,int cost,int attack,int defence,
+                                                 std::string trigger_desc);
+
+// Returns a vector of strings to display a minion with an activated ability
+card_template_t display_minion_activated_ability(std::string name,int cost,int attack,int defence,
+                                                 int ability_cost, std::string ability_desc);
+
+// Returns a vector of strings to display a ritual
+card_template_t display_ritual(std::string name,int cost,int ritual_cost,std::string ritual_desc,
+                               int ritual_charges);
+
+// Returns a vector of strings to display a spell
+card_template_t display_spell(std::string name,int cost,std::string desc);
+
+// Returns a vector of strings to display an enchantment with values for attack and defence
+card_template_t display_enchantment_attack_defence(std::string name,int cost,std::string desc,
+                                                   std::string attack,std::string defence);
+
+// Returns a vector of strings to display a simple enchantment
+card_template_t display_enchantment(std::string name,int cost,std::string desc);
+
+// Returns a vector of strings to display the player information
+// Note: player_num must be 1 or 2
+card_template_t display_player_card(int player_num,std::string name,int life,int mana);
+
+
+/*--- CARD TEMPLATES ---*/
+
+extern const card_template_t CARD_TEMPLATE_MINION_NO_ABILITY;
+extern const card_template_t CARD_TEMPLATE_MINION_WITH_ABILITY;
+extern const card_template_t CARD_TEMPLATE_BORDER;
+extern const card_template_t CARD_TEMPLATE_EMPTY;
+extern const card_template_t CARD_TEMPLATE_RITUAL;
+extern const card_template_t CARD_TEMPLATE_SPELL;
+extern const card_template_t CARD_TEMPLATE_ENCHANTMENT_WITH_ATTACK_DEFENCE;
+extern const card_template_t CARD_TEMPLATE_ENCHANTMENT;
+
+extern const card_template_t PLAYER_1_TEMPLATE;
+extern const card_template_t PLAYER_2_TEMPLATE;
+
+extern const std::vector<std::string> CENTRE_GRAPHIC;
+
+extern const std::string EXTERNAL_BORDER_CHAR_UP_DOWN;
+extern const std::string EXTERNAL_BORDER_CHAR_LEFT_RIGHT;
+extern const std::string EXTERNAL_BORDER_CHAR_TOP_LEFT;
+extern const std::string EXTERNAL_BORDER_CHAR_TOP_RIGHT;
+extern const std::string EXTERNAL_BORDER_CHAR_BOTTOM_LEFT;
+extern const std::string EXTERNAL_BORDER_CHAR_BOTTOM_RIGHT;
