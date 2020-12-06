79a6e4243eb6aecd82bbd2e39f47ce79ebd3cb60
MustansirS <must@Mustansir.localdomain>
2020-12-06 02:05:26 -0500

    added fields and methods to player.h

--- player.h
+++ player.h
@@ -2,15 +2,22 @@
 #define PLAYER_H
 
 #include <string>
-// [TODO] include card object
+#include <vector>
+#include "minion.h"
+#include "ritual.h"
 
 class Player
 {
-public: // will make it public now for testing in main
+private:
   std::string name;
+  std::vector<std::shared_ptr<Card>> deck;
+  std::vector<std::shared_ptr<Card>> hand;
+  std::vector<std::shared_ptr<AbstractMinion>> minions;
+  std::vector<std::shared_ptr<AbstractMinion>> graveyard;
+  std::shared_ptr<Ritual> ritual;
 public:
   Player(std::string name);
-  ~Player();
+  ~Player(); // I don't think you'll need to implement a dtor
   void startTurn();
   void endTurn();
   void attack(int i, Player *p);
@@ -19,6 +26,11 @@ public:
   void play(int i, int j, Player *p);
   void use(int i);
   void use(int i, int j, Player *p);
+  Minion* getMinion(int i);
+  Ritual* getRiutal();
+  void setRitual(std::shared_ptr<Ritual> ritual);
+  void moveToHand(int i); // ith minion goes back to hand
+  void resurrect();
 };
 
 #endif
