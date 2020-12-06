e1ee4b94ae3458849e8b9ba5705387300e932152
MustansirS <must@Mustansir.localdomain>
2020-12-06 02:01:34 -0500

    added methods and fields to board

--- board.h
+++ board.h
@@ -1,13 +1,17 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#include "player.h"
+
 class Board
 {
 private:
-  /* data */
+  int active;
+  std::shared_ptr<Player> player1;
+  std::shared_ptr<Player> player2;
 public:
-  Board(/* args */);
-  ~Board();
+  Board(std::shared_ptr<Player>,std::shared_ptr<Player>);
+  ~Board(); // I don't think you'll need to implement a dtor
   void endturn();
   void attack(int i);
   void attack(int i, int j);
@@ -15,6 +19,7 @@ public:
   void play(int i, int p, int t);
   void use(int i);
   void use(int i, int p, int t);
+  Player* getPlayer(bool active = true); // if false is passed, return inactive player
   void inspect(int i);
   void display();
   void showHand();
