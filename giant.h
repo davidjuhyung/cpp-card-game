bb18942585e5e457e839ddf16871d6373327c789
MustansirS <must@Mustansir.localdomain>
2020-12-07 19:40:07 -0500

    changed

--- giant.h
+++ giant.h
@@ -5,7 +5,7 @@
 
 class GiantStrength public : Enchantment {
   public:
-  	GiantStrength(std::string name = "GiantStrength", Board* board, Minion* minion = nullptr);
+  	GiantStrength(std::string name = "Giant Strength", Board* board, Minion* minion = nullptr);
   	void useAbility(int activeplayer) override;
   	void useTriggered(int activeplayer) override;
 };
