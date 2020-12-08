2242c8f1024cacd5ec8322c1231ca40196928f80
MustansirS <must@Mustansir.localdomain>
2020-12-08 14:55:59 -0500

    review

--- enchantment.h
+++ enchantment.h
@@ -7,6 +7,7 @@ class Enchantment public : AbstractMinion {
 	std::shared_ptr<AbstractMinion> minion;
   public:
   	Enchantment(std::string name, Board* board, std::shared_ptr<AbstractMinion> minion = nullptr);
+  	std::shared_ptr<AbstractMinion> getMinion();
     void play(int player, int minion) override;
 };
 
