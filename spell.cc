18090d8713e2eaa8e41f2e58597d912514dcc5fb
r9chu <r9chu@student.cs.uwaterloo.ca>
2020-12-08 14:51:55 -0500

    update 2

--- spell.cc
+++ spell.cc
@@ -1,20 +1,21 @@
 #include "spell.h"
 
-void Spell::play(nt player, int minion) {
+void Spell::play(int player, int minion) {
     if (description == "Banish"){
         cost -= 2;
-        //Minion temp{} //what values??;
+        //Minion temp{} 
         //auto m = std::make_shared<Minion>(temp);
         graveyard.push_back(minion);
         //destroy target minion or ritual
     }
     else if (description == "Unsommon"){
         cost --;
+        board->getPlayer(player)->moveToHand(minion);
         //return target minion to its owner's hand
     }
     else if (description == "Recharge"){
         cost --;
-        getRitual().charges += 3;
+        board->getPlayer(player)->getRitual().charges += 3;
         //ritual gains 3 charges
     }
     else if (description == "Disenchant"){
@@ -24,7 +25,7 @@ void Spell::play(nt player, int minion) {
     }
     else if (description == "RaiseDead"){
         cost--;
-        top_minion = graveyard.back().c;
+        board->getPlayer(player)->resurect();
         top_minion.defence = 1;
         //resurrect the top minion in your graveyard and set its defence to 1
     }
@@ -32,10 +33,11 @@ void Spell::play(nt player, int minion) {
         cost--;
         play_one_mininion = getMinion(1);
         play_two_mininion = getMinion(2);
-        
+        play_one_num = numMinions(1);
+        play_two_num = numMinions(2);
+        for (int i; i < play_one_num; i++) {
+            
+        }
         //deal 2 damage to all minion
-    }
-    else if (description == "Summoner"){
-        
     }
 }
