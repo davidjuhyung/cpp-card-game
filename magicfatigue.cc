ea5eb1aeb18dcca337320705cd153b7533377846
r9chu <r9chu@student.cs.uwaterloo.ca>
2020-12-08 21:17:22 -0500

    activation cost added

--- magicfatigue.cc
+++ magicfatigue.cc
@@ -4,7 +4,6 @@ MagicFatigue::MagicFatigue(std::string name = "Magic Fatigue", Board* board) : E
 
 void MagicFatigue::play(int player, int minion){
     //enchanted minion's activated ability costs 2 more
-    //where activated ability cost?
     auto m = std::make_share<MagicFatigue>(name,board);
     m->minion = board->getPlayer(player)->getMinion(minion);
 	m->actions = m->minion->getAction();
@@ -12,11 +11,8 @@ void MagicFatigue::play(int player, int minion){
 	m->defence = m->minion->getDefence();
 	m->gainAction = m->minion->gaining();
 	m->name = m->minion->getName();
+	m->activationCost = m->minion->getActivationCost() + 2;
     board->getPlayer(player)->replaceMinion(minion,m);
 }
-void MagicFatigue::useAbility(int activeplayer, int target) {
-    minion->useAbility(activeplayer,target);
-}
-void MagicFatigue::useTriggered(int activeplayer, int target) {
-    minion->useTriggered(activeplayer,target);
-}
+void MagicFatigue::useAbility(int activeplayer, int target) { minion->useAbility(activeplayer,target); }
+void MagicFatigue::useTriggered(int activeplayer, int target) { minion->useTriggered(activeplayer,target); }
