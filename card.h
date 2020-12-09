d08a4431d1c9626a70f03f2c6f0b6708bde0dd51
MustansirS <must@Mustansir.localdomain>
2020-12-09 15:33:27 -0500

    changed play param

--- card.h
+++ card.h
@@ -21,9 +21,9 @@ class Card
   	
   	int getCost() const { return cost; }
   	
-  	// always pass in target player as argument in play. This function is the play method for the card class. It
+  	// This function is the play method for the card class. It
     // effects the board in some way depending on the type and specification
-  	virtual void play(int player, int minion = 0, bool actOnRitual = false) = 0;
+  	virtual void play(int owner, int targetPlayer, int minion = 0, bool actOnRitual = false) = 0;
 };
 
 #endif
