11051b1f98a896a124244dd9f6370082eb3e69f2
r9chu <r9chu@student.cs.uwaterloo.ca>
2020-12-08 20:21:33 -0500

    enrage files

---
+++ enrage.h
@@ -0,0 +1,20 @@
+#ifndef ENRAGE_H
+#define ENRAGE_H
+
+#include "enchantment.h"
+
+class Enrage : public Enchantment
+{
+  
+  public:
+
+  	Enrage(std::string name = "Enrage", Board* board);
+
+  	void play(int player, int minion = 0, bool actOnRitual = false) override;
+
+  	void useAbility(int targetplayer, int target) override;
+
+  	void useTriggered(int targetplayer, int target) override;
+};
+
+#endif
